7eec8dafab759bfbb73c4f3d8b80421fc86518a0
freedom5wind <freedo5wnd@gmail.com>
2018-01-15 04:54:31 -0800

    2018-1-15 20:54

--- my_responser.py
+++ my_responser.py
@@ -16,22 +16,98 @@ partner_key = 'hlQtoKynWKTvG5/PGiQL6UxAit9ABu3yK7Hvew0U0uSxTovXqGeEDz3Tw9iVyDcmb
 
 def responser(json_object):
 	request_type = json_object['request_type']
-	if(request_type != 'request_type_sign_in'):
-		# authenticate the request
-		if(json_object['authentication'] not in authentication_table):
-			json_reply_object = {}
-			json_reply_object['return_code'] = 0
-			json_reply_object['error_message'] = 'Authentication failed.'
-			json_reply_object['data'] = {}
-			return json.dumps(json_reply_object)
+#	if(request_type != 'request_type_sign_in'):
+#		# authenticate the request
+#		if(json_object['authentication'] not in authentication_table):
+#			json_reply_object = {}
+#			json_reply_object['return_code'] = 0
+#			json_reply_object['error_message'] = 'Authentication failed.'
+#			json_reply_object['data'] = {}
+#			return json.dumps(json_reply_object)
 	#call the responser
-	return responser_table[request_type](json_object['data'])
+	return responser_table[request_type](eval(json_object['data']))
+
+
+
+# -----------------------------------------------------------------------------
+# Function that convert time from string to UNIX timestamp
+def time_string_to_unix_timestamp(string):
+	format = '%Y-%m-%d %H:%m'
+	return int(time.mktime(time.strptime(string, format)))
+
+
+
+# -----------------------------------------------------------------------------
+# Function that convert time from UNIX timestamp to strng
+def time_unix_timestamp_to_string(timestamp):
+	format = '%Y-%m-%d %H:%m'
+	return time.strftime(format, time.localtime(timestamp))
+
+
+
+# -----------------------------------------------------------------------------
+# Function that calculate sign for the request to Baijiacloud server
+def calculate_sign(call_api_dict):
+	global partner_key
+
+	# assemble the request parameters
+	key_table = sorted(iter(call_api_dict))
+	request_string = ''
+	for i in key_table:
+		request_string += i + '=' + call_api_dict[i] + '&'
+	request_string += 'partner_key=' + partner_key
+	
+	# calculate the md5	
+	md5_obj = hashlib.md5()
+	md5_obj.update(request_string.encode('utf-8'))
+	return md5_obj.hexdigest()
+
+
+
+# -----------------------------------------------------------------------------
+# Function that generate call-api-dict
+def generate_call_api_dict(data):
+	global partner_id
+
+	# assemble the request data
+	call_api_dict = {}
+	call_api_dict['partner_id'] = partner_id
+	# put data in data to the dict
+	for i in data:
+		call_api_dict[i] = data[i]
+	# convert the time into UNIX timestamp if needed
+	if('start_time' in call_api_dict):
+		call_api_dict['start_time'] = time_string_to_unix_timestamp(call_api_dict['start_time'])
+	if('end_time' in call_api_dict):
+		call_api_dict['end_time'] = time_string_to_unix_timestamp(call_api_dict['end_time'])
+	call_api_dict['timestamp'] = int(time.time())
+	# calculate the sign used to call API
+	call_api_sign = calculate_sign(call_api_dict)
+	call_api_dict['sign'] = call_api_sign
+
+	return call_api_dict
+
+
+
+# -----------------------------------------------------------------------------
+# Function that send the request to the Baijiacloud server
+def send_request(call_api_dict, url):
+	# post the create-room-request to the Baijiacloud server
+	request_data = urllib.parse.urlencode(call_api_dict)
+	request_data = request_data.encode('ascii')
+	request_result = urllib.request.urlopen(url, request_data)
+	# get the return json object in the string format
+	result_string = request_result.read().decode('utf-8')
+	result = json.loads(result_string)
+
+	return result
 
 
 
 # -----------------------------------------------------------------------------
 # responser for sign-in-request
 def sign_in_responser(data=None):
+
 	# open the account file
 	if(data['sign_in_type']):	# login as a student
 		account_file = open('student_account')
@@ -74,31 +150,16 @@ responser_table['request_type_sign_in'] = sign_in_responser
 # -----------------------------------------------------------------------------
 #responser for create-room-request
 def create_room_responser(data=None):
-	# assemble the request data
-	call_api_dict = {}
-	call_api_dict['title'] = data['title']
-	call_api_dict['start_time'] = time_string_to_unix_timestamp(data['start_time'])
-	call_api_dict['end_time'] = time_string_to_unix_timestamp(data['end_time'])
-	call_api_dict['timestamp'] = int(time.time())
-	call_api_dict['partner_id'] = partner_id
-	# calculate the sign used to call API
-	call_api_sign = calculate_sign(call_api_dict)
-	call_api_dict['sign'] = call_api_sign
 
-	# post the create-room-request to the Baijiacloud server
-	request_data = urllib.parse.urlencode(call_api_dict)
-	request_data = request_data.encode('ascii')
-	url_create_room = 'https://api.baijiayun.com/openapi/room/create'
-	request_result = urllib.request.urlopen(url_create_room, request_data)
-	# get the return json object in the string format
-	result_string = request_result.read().decode('utf-8')
-	result_object = json.loads(result_string)
+	call_api_dict = generate_call_api_dict(data)
+	url = 'https://api.baijiayun.com/openapi/room/create'
+	result = send_request(call_api_dict, url)
 
 	#assemble the response info
 	json_reply_object = {}
-	json_reply_object['return_code'] = 1
-	json_reply_object['error_message'] = ''
-	json_reply_object['data'] = {'room_id': result_object['room_id']} 
+	json_reply_object['return_code'] = result['code']
+	json_reply_object['error_message'] = result['msg']
+	json_reply_object['data'] = {'room_id': result['data']['room_id']} 
 
 	return json.dumps(json_reply_object)
 	
@@ -109,42 +170,89 @@ responser_table['request_type_create_room'] = create_room_responser
 # -----------------------------------------------------------------------------
 #responser for update-room-request
 def update_room_responser(data=None):
-	# assemble the requset data
-	call_api_dict = {}
+
+	call_api_dict = generate_call_api_dict(data)
+	url = 'https://api.baijiayun.com/openapi/room/update'
+	result = send_request(call_api_dict, url)
+
+	#assemble the response info
+	json_reply_object = {}
+	json_reply_object['return_code'] = result['code']
+	json_reply_object['error_message'] = result['msg']
+	json_reply_object['data'] = {} 
+
+	return json.dumps(json_reply_object)
 
 responser_table['request_type_update_room'] = update_room_responser
 
 
 
 # -----------------------------------------------------------------------------
-#responser for delete room
+#responser for delete-room-request
 def delete_room_responser(data=None):
-	pass
+
+	call_api_dict = generate_call_api_dict(data)
+	url = 'https://api.baijiayun.com/openapi/room/delete'
+	result = send_request(call_api_dict, url)
+
+	#assemble the response info
+	json_reply_object = {}
+	json_reply_object['return_code'] = result['code']
+	json_reply_object['error_message'] = result['msg']
+	json_reply_object['data'] = {} 
+
+	return json.dumps(json_reply_object)
+
 responser_table['request_type_delete_room'] = delete_room_responser
 
 
 
 # -----------------------------------------------------------------------------
-# Function that convert time from string to UNIX timestamp
-def time_string_to_unix_timestamp(string):
-	format = '%Y-%m-%d %H:%m'
-	return int(time.mktime(time.strptime(string, format)))
+# responser for get-room-info-request
+def get_room_info_responser(data=None):
+
+	call_api_dict = generate_call_api_dict(data)
+	url = 'https://api.baijiayun.com/openapi/room/info'
+	result = send_request(call_api_dict, url)
+
+	#assemble the response info
+	json_reply_object = {}
+	json_reply_object['return_code'] = result['code']
+	json_reply_object['error_message'] = result['msg']
+	del result['data']['admin_code']
+	del result['data']['teacher_code']
+	json_reply_object['data'] = result['data']
+
+	return json.dumps(json_reply_object)
+
+responser_table['request_type_get_room_info'] = get_room_info_responser
 
 
 
 # -----------------------------------------------------------------------------
-# Function that calculate sign for the request to Baijiacloud server
-def calculate_sign(call_api_dict):
-	global partner_key
+# responser for get-room-list-request
+def get_room_list_responser(date=None):
 
-	# assemble the request parameters
-	key_table = sorted(iter(call_api_dict))
-	request_string = ''
-	for i in key_table:
-		request_string += i + '=' + call_api_dict[i] + '&'
-	request_string += 'partner_key=' + partner_key
-	
-	# calculate the md5	
-	md5_obj = hashlib.md5()
-	md5_obj.update(request_string.encode('utf-8'))
-	return md5_obj.hexdigest()
+	call_api_dict = generate_call_api_dict(data)
+	url = 'https://api.baijiayun.com/openapi/room/info'
+	result = send_request(call_api_dict, url)
+
+	#assemble the response info
+	json_reply_object = {}
+	json_reply_object['return_code'] = result['code']
+	json_reply_object['error_message'] = result['msg']
+	for i in result['data']['list']:
+		del i['admin_code']
+		del i['teacher_code']
+		del i['student_code']
+		i['start_time'] = time_unix_timestamp_to_string(i['start_time'])
+		i['end_time'] = time_unix_timestamp_to_string(i['end_time'])
+		i['create_time'] = time_unix_timestamp_to_string(i['create_time'])
+	json_reply_object['data'] = result['data']
+
+	return json.dumps(json_reply_object)
+
+responser_table['request_type_get_room_info'] = get_room_info_responser
+
+
+
--- my_server.py
+++ my_server.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def my_server():
 	if request.method == 'POST':
-		# json_string = request.form['json_string']
-		# json_object = json.loads(json_string)
-		# return responser(json_object)
 		return responser(request.form)
+	else:
+		return "Please request data by 'POST'"
